fa5fe274645f87f4794022ca0137e44721c3e529
Kate Drew <katedrew2103@hotmail.co.uk>
2024-11-26 14:59:43 +0000

    completed task 6

--- error- handlers.js
+++ error- handlers.js
@@ -1,6 +1,6 @@
 //postgres errors
 exports.psqlErrorHandler = (err, req, res, next) => {
-  if (err.code === "22P02") {
+  if (err.code === "22P02" || err.code === "23502" || err.code === "23503") {
     res.status(400).send({ msg: "400: Bad request" });
   } else {
     next(err);
